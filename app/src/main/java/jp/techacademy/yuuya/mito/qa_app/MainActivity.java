c9ab8505a9bb48c53878d5d27a3ed586731fd73f
Yuya@Windows <yuya.m.tokai@gmail.com>
2018-02-06 18:00:04 +0900

    3nd Commit (Drawer work)

--- app/src/main/java/jp/techacademy/yuuya/mito/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/yuuya/mito/qa_app/MainActivity.java
@@ -3,7 +3,10 @@ package jp.techacademy.yuuya.mito.qa_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,12 +18,15 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private int mGenre = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +42,43 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //ナビゲーションドロワーの設定
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_hobby) {
+                    mToolbar.setTitle("趣味");
+                    mGenre = 1;
+                } else if (id == R.id.nav_life) {
+                    mToolbar.setTitle("生活");
+                    mGenre = 2;
+                } else if (id == R.id.nav_health) {
+                    mToolbar.setTitle("健康");
+                    mGenre = 3;
+                } else if (id == R.id.nav_compter) {
+                    mToolbar.setTitle("コンピューター");
+                    mGenre = 4;
+                }
+
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
